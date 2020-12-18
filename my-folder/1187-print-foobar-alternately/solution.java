e209145e4d41ad10f74f9aad1911464cb1010318
Tiyasha Mitra <35894987+TiyashaP@users.noreply.github.com>
2020-12-18 16:39:02 +0000

    [LeetCode Sync] Runtime - 17 ms (100.00%), Memory - 39.5 MB (100.00%)

---
+++ my-folder/1187-print-foobar-alternately/solution.java
@@ -0,0 +1,28 @@
+class FooBar {
+    private int n;
+    Semaphore fooSemaphore;
+    Semaphore barSemaphore;
+    public FooBar(int n) {
+        this.n = n;
+        this.fooSemaphore=new Semaphore(1);
+        this.barSemaphore=new Semaphore(0);
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            fooSemaphore.acquire();
+        	printFoo.run();
+            barSemaphore.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            barSemaphore.acquire();
+        	printBar.run();
+            fooSemaphore.release();
+        }
+    }
+}

a43c5aec95e2c68ca35ea086c75efcfaaaad0f7f
Tiyasha Mitra <35894987+TiyashaP@users.noreply.github.com>
2021-01-06 20:50:53 +0000

    [LeetCode Sync] Runtime - 63 ms (24.23%), Memory - 38.6 MB (100.00%)

---
+++ my-folder/0300-longest-increasing-subsequence/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] lis = new int[nums.length];
+        for (int i=0;i<lis.length;i++)
+            lis[i]=1;
+        for (int i = 1; i < nums.length; i++) {
+            {for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && lis[i] < lis[j] + 1)
+                    lis[i] = lis[j] + 1;
+            }
+            }
+        }
+        Arrays.sort(lis);
+        return lis[lis.length-1];
+    
+        
+    }
+}

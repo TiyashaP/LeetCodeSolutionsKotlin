c05bc3da1af9f2e8aa9edc21eedcbe67dba3db2b
Tiyasha Mitra <35894987+TiyashaP@users.noreply.github.com>
2021-01-15 16:45:00 +0000

    [LeetCode Sync] Runtime - 3 ms (0.32%), Memory - 36.1 MB (100.00%)

--- my-folder/0070-climbing-stairs/solution.java
+++ my-folder/0070-climbing-stairs/solution.java
@@ -1,20 +1,11 @@
 class Solution {
-    public int climbStairs(int n) {
-        
-        int[] noOfFloors=new int[n+1]; 
-        if(n==0)
-            return 0;
-         if(n==1)
-            return 1;
-        else{
-        noOfFloors[0]=0;
-        noOfFloors[1]=1;
-        noOfFloors[2]=2;
-        }
-        for(int i=3;i<=n;i++)
-        {
-            noOfFloors[i]=noOfFloors[i-1]+noOfFloors[i-2];
-        }
-        return noOfFloors[n];
+   public int climbStairs(int n) {
+        Map<Integer,Integer> climbStairMap=new HashMap<>();
+        climbStairMap.put(1,1);
+        climbStairMap.put(0,0);
+        climbStairMap.put(2,2);
+        IntStream.iterate(3,i->i<=n,i->i+1)
+                .forEach(elem->climbStairMap.computeIfAbsent(elem,ignore->climbStairMap.get(elem-1)+climbStairMap.get(elem-2)));
+       return climbStairMap.get(n);
     }
 }

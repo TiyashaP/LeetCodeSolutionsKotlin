0f7eb5376d247e74243a00deafe6c45dde88b20e
Tiyasha Mitra <35894987+TiyashaP@users.noreply.github.com>
2022-09-26 18:42:58 +0000

    [LeetCode Sync] Runtime - 12 ms (0.00%), Memory - 42.7 MB (100.00%)

--- my-folder/1316-fizz-buzz-multithreaded/solution.java
+++ my-folder/1316-fizz-buzz-multithreaded/solution.java
@@ -1,93 +1,97 @@
-
-
-
-
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.function.IntConsumer;
-
 class FizzBuzz {
     private int n;
-    private int count;
-    private Lock lock;
-    		
+    private int i;
+    private ReentrantLock lock;
+
     public FizzBuzz(int n) {
         this.n = n;
-        this.count=1;
-        this.lock=new ReentrantLock();
+        this.i=1;
+        lock=new ReentrantLock(true);
     }
 
     // printFizz.run() outputs "fizz".
     public void fizz(Runnable printFizz) throws InterruptedException {
-    	
-    	while(count<=n)
-    	{
-    	lock.lock();
-    	if(count%3==0 && count%5!=0 && count<=n)
-    	{
-    		count++;
-    		printFizz.run();
-            
-    	}
-    	lock.unlock();
-    	}
-    	
-        
+         while(true)
+        {
+    lock.lock();
+       
+        if(i>n)
+        {
+            lock.unlock();
+            break;
+        }
+            if(i%3==0 && i%5!=0)
+            {
+               printFizz.run();
+               i++;
+                
+            }
+       lock.unlock(); 
+        }
+       
     }
 
     // printBuzz.run() outputs "buzz".
     public void buzz(Runnable printBuzz) throws InterruptedException {
-    	
-    	while(count<=n)
+         while(true)
         {
-    	lock.lock();
-    	if(count%5==0 && count%3!=0 && count<=n)
-    	{
-    		count++;
-    		printBuzz.run();
-            
-    	}
-    	lock.unlock();
+     lock.lock();
+       
+        if(i>n)
+        {
+            lock.unlock();
+            break;
         }
-    	
-        
-        
+            if(i%3!=0 && i%5==0)
+            {
+               printBuzz.run();
+                i++;
+                
+            }
+       lock.unlock(); 
+        }
+       
     }
 
     // printFizzBuzz.run() outputs "fizzbuzz".
     public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
-    	
-    	while(count<=n)
+        while(true)
         {
-    	lock.lock();
-    	if(count%3==0 && count%5==0 && count<=n)
-    	{
-    		count++;
-    		printFizzBuzz.run();
-            
-    	}
-    	lock.unlock();
+         lock.lock();
+        
+        if(i>n)
+        {
+            lock.unlock();
+            break;
         }
-    	
-        
-        
+            if(i%3==0 && i%5==0)
+            {
+               printFizzBuzz.run();
+               i++; 
+            }
+          lock.unlock(); 
+        }
+       
     }
 
     // printNumber.accept(x) outputs "x", where x is an integer.
     public void number(IntConsumer printNumber) throws InterruptedException {
-    	
-    	while(count<=n)
-    	{lock.lock();
-    		if(count%3!=0 && count%5!=0 && count<=n)
-    	{
-    		printNumber.accept(count);
-            count++;
-    	}
-    		lock.unlock();
-    	}
-    	
+        while(true)
+        {
+         lock.lock();
         
-        
+        if(i>n)
+        {
+            lock.unlock();
+            break;
+        }
+            if(i%3!=0 && i%5!=0)
+            {
+               printNumber.accept(i);
+               i++;
+            }
+        lock.unlock(); 
+        }
+      
     }
 }
-

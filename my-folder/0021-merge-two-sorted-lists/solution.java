52595dd8e1357358eb354596f900c01e2a3d7ecc
Tiyasha Mitra <35894987+TiyashaP@users.noreply.github.com>
2022-10-18 17:48:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (99.99%)

--- my-folder/0021-merge-two-sorted-lists/solution.java
+++ my-folder/0021-merge-two-sorted-lists/solution.java
@@ -10,25 +10,61 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        final ListNode root=new ListNode();
-        ListNode prev=root;
+        ListNode head=null;
+        ListNode current=null;
+         ListNode prev=null;
         while(list1!=null && list2!=null)
         {
             if(list1.val<list2.val)
             {
-                prev.next=list1;
-                list1=list1.next;
-                
+                  current=new ListNode(list1.val,null);  
+                  list1=list1.next;
+        
             }
             else{
-                prev.next=list2;
+                current=new ListNode(list2.val,null);
                 list2=list2.next;
                 
             }
-            prev=prev.next;
+            if(head==null)
+            {head=current;
+             prev=head;
+            }
+            else{
+                 prev.next=current;
+                 prev=current;
+            }
+            
         }
-        prev.next=list1!=null?list1:list2;
-        return root.next;
         
+         while(list1!=null)
+        {
+            current=new ListNode(list1.val,null);  
+            list1=list1.next;
+            if(head==null)
+            {head=current;
+             prev=head;
+            }
+            else{
+                 prev.next=current;
+                 prev=current;
+            }
+        
+        }
+         while(list2!=null)
+        {
+            current=new ListNode(list2.val,null);  
+            list2=list2.next;
+            if(head==null)
+            {head=current;
+             prev=head;
+            }
+            else{
+                 prev.next=current;
+                 prev=current;
+            }
+        }
+        
+    return head;
     }
 }

014af92379134919c29594b4f7735e1816df94d4
Tiyasha Mitra <35894987+TiyashaP@users.noreply.github.com>
2022-12-25 12:11:10 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 42.7 MB (100.00%)

--- my-folder/0039-combination-sum/solution.java
+++ my-folder/0039-combination-sum/solution.java
@@ -1,28 +1,30 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> all=new ArrayList<>();
-        combinationSumRec(0,candidates,target,new ArrayList<>(),all);
-        return all;
+        List<List<Integer>> allCombination=new ArrayList<>();
+        List<Integer> currentCombination=new ArrayList<Integer>();
+        combinationSumRec(candidates,target,allCombination,currentCombination,0);
+        return allCombination;
+        
     }
     
-     public List<List<Integer>> combinationSumRec(int index,int[] candidates, int target,List<Integer> ds,List<List<Integer>> all) {
-         if(index==candidates.length)
-         {
-             if(target==0)
-             {
-                 all.add(new ArrayList(ds));
-             }
-             return all;
-                 
-         }
-         if(target>=candidates[index])
-         {
-             ds.add(candidates[index]);
-             combinationSumRec(index,candidates,target-candidates[index],ds,all);
-             ds.remove(ds.size()-1);
-         }
-         
-         return combinationSumRec(index+1,candidates,target,ds,all);
-         
+    public void combinationSumRec(int[] candidates, int target,List<List<Integer>> allCombination,List<Integer> currentCombination,int index) {
+        if(index>=candidates.length)
+            return;
+        if(target==0)
+        {
+            List<Integer> combinationToAdd=new ArrayList<>(currentCombination);
+            allCombination.add(combinationToAdd);
+            return;
+        }
+        
+        if(candidates[index]<=target)
+        {
+            currentCombination.add(candidates[index]);
+            combinationSumRec(candidates,target-candidates[index],allCombination,currentCombination,index);
+            currentCombination.remove(currentCombination.size()-1);
+        }
+         combinationSumRec(candidates,target,allCombination,currentCombination,index+1);
+        
+        
     }
 }

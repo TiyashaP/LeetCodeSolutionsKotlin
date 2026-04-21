425b726a639c0dc8bcb5acc3a868d5db224537a2
Tiyasha Mitra <35894987+TiyashaP@users.noreply.github.com>
2026-04-21 21:39:51 +0000

    [LeetCode Sync] Runtime - 2 ms (99.53%), Memory - 48 MB (39.39%)

--- my-folder/0001-two-sum/solution.kt
+++ my-folder/0001-two-sum/solution.kt
@@ -1,13 +1,13 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val indexMap=mutableMapOf<Int, Int>()
-        nums.forEachIndexed{i,num->
-         indexMap[target-num]?.let{
-            return intArrayOf(it,i)
-         }
-            indexMap[num]=i
-         }
-    throw IllegalArgumentException("No two sum solution")
-        
+        var indexMap=mutableMapOf<Int,Int>()
+        nums.forEachIndexed{ i,v ->
+        val remainder=target-v
+        if(remainder in indexMap)
+         return intArrayOf(i,indexMap[remainder]!!)
+        else
+          indexMap[v]=i
+        }
+        return intArrayOf()
     }
 }
